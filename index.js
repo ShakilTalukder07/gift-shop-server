0a60a35a36031bf3e3888ee5cd275babb2efc0d0
ShakilTalukder07 <shakil.talukder07@gmail.com>
2023-04-08 16:35:59 +0600

    image changed

--- index.js
+++ index.js
@@ -9,7 +9,25 @@ app.use(express.json());
 const categories = require("./data/category.json");
 const products = require("./data/products.json");
 
+app.get("/productCategory", (req, res) => {
+    res.send(categories);
+})
 
+app.get("/category/:id", (req, res) => {
+    const id = req.params.id;
+    const productsCategory = products.filter((p) => p.category_id === id);
+    res.send(productsCategory);
+});
+
+app.get("/products", (req, res) => {
+    res.send(products);
+});
+
+app.get("/products/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedProduct = products.find( p => p._id === id)
+    res.send(selectedProduct)
+});
 
 app.get("/", (req, res) => {
     res.send("Server Is Ready.");
